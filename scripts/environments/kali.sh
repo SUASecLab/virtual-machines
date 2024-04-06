296835ce16baea90e3d3b8b972911a5a172df9b2
Tobias Tefke <tobias@tefke.name>
2024-04-06 20:21:13 +0200

    kali: install lzop

--- scripts/environments/kali.sh
+++ scripts/environments/kali.sh
@@ -5,3 +5,6 @@ cat >>/etc/sudoers.d/kali <<EOF
 laboratory laboratory = (root) NOPASSWD: /usr/bin/wireshark
 laboratory laboratory = (root) NOPASSWD: /usr/bin/msfdb
 EOF
+
+# Install lzop
+apt-get install lzop -y
