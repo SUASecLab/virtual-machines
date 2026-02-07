5966fee6e5935044bd890ec72affe077c5633114
Tobias Tefke <tobias@tefke.name>
2026-02-07 11:27:07 +0100

    Rework change password script

--- change_password.sh
+++ change_password.sh
@@ -10,15 +10,15 @@ fi
 # Change pw in preseeds
 sed -i "s|TH3P455W0RD|${1}|g" http/kali-preseed.cfg
 sed -i "s|TH3P455W0RD|${1}|g" http/debian-preseed.cfg
+sed -i "s|TH3P455W0RD|${1}|g" http/suasploitable-basic-preseed.cfg
 
 # Change in packer files
 sed -i "s|TH3P455W0RD|${1}|g" heaven.pkr.hcl
 sed -i "s|TH3P455W0RD|${1}|g" heaven-exam.pkr.hcl
 sed -i "s|TH3P455W0RD|${1}|g" iotlab.pkr.hcl
-sed -i "s|TH3P455W0RD|${1}|g" kali.pkr.hcl
-sed -i "s|TH3P455W0RD|${1}|g" http/debian-preseed.cfg
 
-sed -i "s|TH3P455W0RD|${1}|g" http/suasploitable-basic-preseed.cfg
+sed -i "s|TH3P455W0RD|${1}|g" kali.pkr.hcl
+sed -i "s|TH3P455W0RD|${1}|g" suasploitable-basic.pkr.hcl
 sed -i "s|TH3P455W0RD|${1}|g" suasploitable-cms.pkr.hcl
 sed -i "s|TH3P455W0RD|${1}|g" suasploitable-cloud.pkr.hcl
 sed -i "s|TH3P455W0RD|${1}|g" suasploitable-devbox.pkr.hcl
